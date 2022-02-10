33da9f0bff1575e2c7fbf56be47c498982783a12
Christian Calderon <ccalderon9411@hotmail.com>
2022-02-10 11:02:16 -0500

    chore(filters): add filter and utils error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-// export public api from here
-// for example:
-// export * from './decorators';
-export * from './greeting/greeting.module';
-export * from './greeting/services/greeting.service';
+export { ExceptionsFilter } from './filters/exceptions.filter';
+export * from './utils/error.utils';
